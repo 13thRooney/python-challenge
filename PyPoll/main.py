6e5673435e95df2450d09f8a358854ecae341bc4
13thRooney <rooneydad@gmail.com>
2019-05-04 19:07:17 -0700

    modfied and completed Pypoll main

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,25 +1,66 @@
-#PyPoll
-#Imort necessary modules
+# import modules
 import os
 import csv
 
-# Get election_data.csv
-csvpathPyPoll = os.path.join("..", "Resources", "election_data.csv")
+# define path
+path_election_data = os.path.join('..','Resources','election_data.csv')
 
-# Store data in lists
-votes []
-candidates []
+# initialize variables
+candidate_votes = {}  # key: cand name, val: vote count
+total_votes = 0
+candidate_list = []
 
-# Open & Read csv file
-with open(csvpathPyRoll, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-     # To skip header line
-    header = next(csvreader)
+# read the csv
+with open(path_election_data,'r') as file_election_data:
+    reader_csv = csv.reader(file_election_data, delimiter=',')
+    skip_header = next(reader_csv)
 
-#Analyze the votes and calculate each of the following:
+    # Loop through data
+    for row in reader_csv:
+        total_votes = total_votes + 1
+        #find name of candidate
+        candidate_name = row[2]
+        # add name to list if not seen before
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+
+        # put count in correct entry
+        if candidate_name not in candidate_votes:
+            candidate_votes[candidate_name] = 1
+        else: # seen this candidate alreday
+            candidate_votes[candidate_name] += 1
+
+CR = '\n'  #named '\n' with variable name
+
+# open output file
+fout_name = 'votes_results.txt'
+fout = open(fout_name, 'w')
+
+s = 'Election Results'
+print(s); fout.write(s + CR)
+s = '-'*20
+print(s); fout.write(s + CR)
 
 #The total number of votes cast
-#A complete list of candidates who received votes
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#The winner of the election based on popular vote.
+s = f"Total Votes: {total_votes}"
+print(s); fout.write(s + CR)
+s = '-'*20
+print(s); fout.write(s + CR)
+
+# results
+winner = None
+for cand, count in sorted(candidate_votes.items(), key=lambda t: t[1], reverse=True):
+    if winner == None:
+        winner = cand
+    pct = (count / total_votes)*100
+    s = f"{cand:10}: {pct:.3f}% ({count})"
+    print(s); fout.write(s + CR)
+s = '-'*20
+print(s); fout.write(s + CR)
+s = f"Winner: {winner}"
+print(s); fout.write(s + CR)
+s = '-'*20
+print(s); fout.write(s + CR)
+fout.close()
+
+
